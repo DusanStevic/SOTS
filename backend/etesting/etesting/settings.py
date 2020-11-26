8ef8374818d1b391298d2ef5bf725555a601e07c
Dusan Stevic <stevicdule@gmail.com>
2020-11-26 12:52:48 +0100

    etest app

--- backend/etesting/etesting/settings.py
+++ backend/etesting/etesting/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
+    'etest',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+
+
