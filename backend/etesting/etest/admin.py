2eda29d29763bed8da91ead9fb5a7a01ff54dcc6
MilicaVojnovic <milica.vojnovic97@fmail.com>
2020-12-08 17:21:30 +0100

    First commit to model

--- backend/etesting/etest/admin.py
+++ backend/etesting/etest/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Etest, Question, Answer, UsersAnswer, TestTaker, Course
+import nested_admin
 
-# Register your models here.
+
+class AnswerInline(nested_admin.NestedTabularInline):
+	model = Answer 
+
+
+class QuestionInline(nested_admin.NestedTabularInline):
+	model = Question
+	inlines = [AnswerInline,]
+
+
+class EtestAdmin(nested_admin.NestedModelAdmin):
+	inlines = [QuestionInline,]
+
+
+class UsersAnswerInline(admin.TabularInline):
+	model = UsersAnswer
+
+
+class TestTakerAdmin(admin.ModelAdmin):
+	inlines = [UsersAnswerInline,]
+
+
+admin.site.register(Etest, EtestAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(TestTaker, TestTakerAdmin)
+admin.site.register(UsersAnswer)
+admin.site.register(Course)
--- backend/etesting/etest/models.py
+++ backend/etesting/etest/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Etest(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, default='')
+    # potrebno je dodati listu profesora koji su kreirali taj test ili kurs
+    # na predthodnom terminu su pricali kako vise prof moze da se doda na jedan test
+    # a neki su samo test vezali za kurs 
+    # mozemo napraviti da prof vezemo za kurs, a onda test za kurs
+    # tako da svaki novi test koji kreiramo i dodamo testu bice automatski dodat i prof koji drze taj kurs tako da 
+    # ce ih oni videti
+
+class Question(models.Model):
+    etest = models.ForeignKey(Etest, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+class TestTaker(models.Model):
+    # ovde treba dodati da je to student
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    test = models.ForeignKey(Etest, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+
+class UsersAnswer(models.Model):
+    test_taker = models.ForeignKey(TestTaker, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
